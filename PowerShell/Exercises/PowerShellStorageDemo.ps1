e75d7df71f543335c99ca46afc68a84fe2c84c5b
Abhishek Anand <aanandmailbox@gmail.com>
2016-10-20 15:57:54 -0700

    StorageDemo

---
+++ PowerShell/Exercises/PowerShellStorageDemo.ps1
@@ -0,0 +1,48 @@
+﻿
+#login to the Account 
+Login-AzureRmAccount
+
+# List all Storage Account 
+Get-AzureRmStorageAccount |select ResourceGroupName,storageAccountName,Location
+
+# Verify Coorect account where you would like to upload
+Get-AzureRmStorageAccount -ResourceGroupName ftdnt-poc-rg -StorageAccountName ftdntweb02vmst
+
+#Get Storage account Key 
+$storageAccoutKey =(Get-AzureRMStorageAccountKey -ResourceGroupName ftdnt-poc-rg -Name ftdntweb02vmst)[0].value
+
+# Get Context where you would like to upload data 
+$storageAccountContext = New-AzureStorageContext -StorageAccountKey $storageAccoutKey -StorageAccountName ftdntweb02vmst
+
+# Create new containers in the storage account 
+New-AzureStorageContainer -Name container1 -Context $storageAccountContext -Permission Off
+
+Get-AzureStorageContainer -Context $storageAccountContext | select Name
+
+##################################################################
+
+# upload data to Azure  
+Set-AzureStorageBlobContent -File "C:\Users\abhanand\Downloads\data\cat1.jpg" -Container container1 -Blob catuploaded.jpg -Context $storageAccountContext -Verbose -debug
+
+##################################################################
+
+# Download the latest version of AzCopy : http://aka.ms/downloadazcopy 
+# Documentaion  : https://azure.microsoft.com/en-us/documentation/articles/storage-use-azcopy/ 
+
+# PS C:\Users\abhanand> cd "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy"
+
+# PS C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy> 
+
+.\AzCopy.exe /Source:C:\Users\abhanand\Downloads\data /Dest:https://ftdntweb02vmst.blob.core.windows.net/container1 /DestKey:l5uKYs2Snck3Ltp9D24N77nlPTIB7MMR/bHBAUrcg40f1KdiL8lgrK3rqpI7TpTrOojInbpYls6tLufQlG73YQ== /S /V
+
+##################################################################
+
+# Uploading VHD using Powershell 
+
+Add-AzureRmVhd -ResourceGroupName ftdnt-poc-rg -Destination https://ftdntweb02vmst.blob.core.windows.net/vhds/uploaded.vhd -LocalFilePath "C:\Users\abhanand\Downloads\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd" -Verbose
+
+##################################################################
+
+# Microsoft Azure Storage Explorer  : http://storageexplorer.com/ 
+
+#################################################
