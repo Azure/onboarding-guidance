6394adc551f3550d593cd86d2938b7d06b5ceec1
Abhishek Anand <aanandmailbox@gmail.com>
2017-01-09 10:29:39 -0800

    quick script update

--- PowerShell/Exercises/PowerShellStorageDemo.ps1
+++ PowerShell/Exercises/PowerShellStorageDemo.ps1
@@ -6,9 +6,12 @@
 #login to the Account 
 Login-AzureRmAccount
 
+$rg = 'ftaz-poc-rg'
+$stname = 'ftazweb01vmst'
+
 # Uploading VHD using Powershell 
 
-Add-AzureRmVhd -ResourceGroupName Storagedemo -Destination https://ftstdemo.blob.core.windows.net/vhds/uploaded.vhd -LocalFilePath "C:\Users\abhanand\Downloads\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd" -Verbose -debug
+Add-AzureRmVhd -ResourceGroupName $rg -Destination https://ftazweb01vmst.blob.core.windows.net/vhds/customuploaded.vhd -LocalFilePath "C:\Users\abhanand\Downloads\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd" -Verbose -debug
 
 #################################################
 
@@ -17,13 +20,13 @@ Add-AzureRmVhd -ResourceGroupName Storagedemo -Destination https://ftstdemo.blob
 Get-AzureRmStorageAccount |select ResourceGroupName,storageAccountName,Location -Verbose -Debug
 
 # Verify Correct storage account details  where you would like to upload data 
-Get-AzureRmStorageAccount -ResourceGroupName Storagedemo -StorageAccountName ftstdemo
+Get-AzureRmStorageAccount -ResourceGroupName $rg -StorageAccountName $stname
 
 #Get Storage account Key 
-$storageAccoutKey =(Get-AzureRMStorageAccountKey -ResourceGroupName Storagedemo -Name ftstdemo)[0].value
+$storageAccoutKey =(Get-AzureRMStorageAccountKey -ResourceGroupName $rg -Name ftazweb01vmst)[0].value
 
 # Get Context where you would like to upload data 
-$storageAccountContext = New-AzureStorageContext -StorageAccountKey $storageAccoutKey -StorageAccountName ftstdemo
+$storageAccountContext = New-AzureStorageContext -StorageAccountKey $storageAccoutKey -StorageAccountName $stname
 
 # Create new containers in the storage account 
 New-AzureStorageContainer -Name container1 -Context $storageAccountContext -Permission Off
@@ -44,7 +47,7 @@ Set-AzureStorageBlobContent -File "C:\Users\abhanand\Downloads\data\cat1.jpg" -C
 
 # PS C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy> 
 
-.\AzCopy.exe /Source:C:\Users\abhanand\Downloads\data /Dest:https://ftstdemo.blob.core.windows.net/container1 /DestKey:l5uKYs2Snck3Ltp9D24N77nlPTIB7MMR/bHBAUrcg40f1KdiL8lgrK3rqpI7TpTrOojInbpYls6tLufQlG73YQ== /S /V
+.\AzCopy.exe /Source:C:\Users\abhanand\Downloads\data /Dest:https://$stname.blob.core.windows.net/container1 /DestKey:<key> /S /V
 
 ##################################################################
 
